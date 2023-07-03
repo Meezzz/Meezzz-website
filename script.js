faf731c536cb34aac3dadcc48469425fcc874218
Meezzz <106698528+Meezzz@users.noreply.github.com>
2023-07-03 22:52:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function onClickMenu() {
+  document.getElementById("menu-bar").classList.toggle("change")
+  document.getElementById("menubarnav").classList.toggle("change")
+  document.getElementById("menu-bg").classList.toggle("change-bg")
+}
